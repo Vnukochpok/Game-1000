6f2b316b6848659a7092ea31b7cf1ec29c4ce3d3
Andrey Zhdanov <CayMan.1@bk.ru>
2025-12-29 10:55:41 +0700

    Made a new game process

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-from logic.logic1000 import throw, Combination, check_unscore
+from logic.logic1000 import throw, check_score, get_normal_score
 
 play = True
 def game(player1, player2):
@@ -9,23 +9,112 @@ def game(player1, player2):
         hod_p1 = True
         hod_p2 = False
 
-        check = Combination()
         k = 5
+        total_score_p1 = 0
+        total_score_p2 = 0
         while hod_p1:
             throwing = throw(k)
-            unscored_cube = check_unscore(throwing)
+            print(throwing)
+            checking_score = check_score(throwing)
+            list_res = get_normal_score(checking_score)
+            print(list_res)
+            score = list_res[0]
+            reroll_dice = list_res[1]
 
-            if check.b_strit(throwing) != 0:
-                score = check.b_strit(throwing)
+            #[0, 5] [0, 2]
+            if score == 0 and (reroll_dice == 5 or reroll_dice < 5):
+                print("Ничего не выпало, ход передается следующему игроку\n ---------------------------------")
+                print(total_score_p1)
+                hod_p1 = False
+                hod_p2 = True
+                break
+
+            #[20, 2]
+            elif score != 0 and reroll_dice < 5:
+                total_score_p1 += score
+                decis = input("Хотите кинуть кубики еще раз? (Y/N)")
+                if decis == 'Y':
+                    k = reroll_dice
+                elif decis == 'N':
+                    print("Ход передается следующему игроку\n ---------------------------------")
+                    print(total_score_p1)
+                    hod_p1 = False
+                    hod_p2 = True
+                    break
+                else:
+                    print("Wrong answer >:(")
+                    break
+
+            #[55, 0]
+            elif score != 0 and reroll_dice == 0:
+                total_score_p1 += score
+                decis = input("Хотите кинуть кубики еще раз? (Y/N)")
+                if decis == 'Y':
+                    k = 5
+
+                elif decis == 'N':
+                    print("Ход передается следующему игроку\n ---------------------------------")
+                    print(total_score_p1)
+                    hod_p1 = False
+                    hod_p2 = True
+                    break
+
+                else:
+                    print("Wrong answer >:(")
+                    break
+
+            else:
+                print("Ошибка")
+
+
+        while hod_p2:
+            throwing = throw(k)
+            checking_score = check_score(throwing)
+            list_res = get_normal_score(checking_score)
+
+            score = list_res[0]
+            reroll_dice = list_res[1]
+
+            if score == 0 and (reroll_dice == 5 or reroll_dice < 5):
+                print("Ничего не выпало, ход передается следующему игроку\n ---------------------------------")
+                print(total_score_p2)
+                hod_p2 = False
                 hod_p1 = True
+                break
 
-            if check.m_strit(throwing) != 0:
-                score = check.b_strit(throwing)
-                hod_p1 = True
+            elif score != 0 and reroll_dice < 5:
+                total_score_p2 += score
+                decis = input("Хотите кинуть кубики еще раз? (Y/N)")
+                if decis == 'Y':
+                    k = reroll_dice
+                elif decis == 'N':
+                    print("Ход передается следующему игроку\n ---------------------------------")
+                    print(total_score_p2)
+                    hod_p2 = False
+                    hod_p1 = True
+                    break
+                else:
+                    print("Wrong answer >:(")
+                    break
 
-            if check.triple(throwing) != 0:
-                score = check.b_strit(throwing)
+            elif score != 0 and reroll_dice == 0:
+                total_score_p2 += score
+                decis = input("Хотите кинуть кубики еще раз? (Y/N)")
+                if decis == 'Y':
+                    k = 5
 
-                dec = input("Хотите бросить еще раз?")
+                elif decis == 'N':
+                    print("Ход передается следующему игроку\n ---------------------------------")
+                    print(total_score_p2)
+                    hod_p2 = False
+                    hod_p1 = True
+                    break
 
+                else:
+                    print("Wrong answer >:(")
+                    break
 
+            else:
+                print("Ошибка")
+
+game("andrey", "polya")
